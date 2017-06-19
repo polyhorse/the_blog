5372013a7e760cd19e0c775bbf9cf1ed03172c84
macduff <macduff@macduff.local>
2017-06-19 15:03:23 -0600

    member dashboard started. has sidebar and main column

--- server/member.py
+++ server/member.py
@@ -1,9 +1,14 @@
 from flask.ext.classy import FlaskView
 from flask_user import login_required
+from flask_themes2 import Themes, render_theme_template, get_theme, get_themes_list
+
+
+from config import Config
 
 
 class MemberView(FlaskView):
 	decorators = [login_required]
+	theme = Config().MEMBER_THEME
 
 	def index(self):
-		return 'hello'
+		return render_theme_template(self.theme, 'index.html')
