68a1c42402184dfa13c3e3ddaaad97cb8f18a63a
macduff <macduff@macduffs-MacBook-Pro.local>
2017-11-02 20:39:32 -0600

    puts upaoded folder in repo and updates requirements

--- server/member.py
+++ server/member.py
@@ -1,4 +1,4 @@
-from flask.ext.classy import FlaskView
+from flask_classy import FlaskView
 from flask_user import login_required
 from flask_themes2 import Themes, render_theme_template, get_theme, get_themes_list
 
